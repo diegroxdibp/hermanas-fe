29b2b9b45ca6bba5b883519fcdcf0a84aa8d4548
DiegoPonte <diego.ponte@xpand-it.com>
2025-01-18 01:53:38 +0000

    basic setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
   title = 'hermanas-fe';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './shared/components/body/body.component';
+import { BiographyComponent } from './shared/components/biography/biography.component';
+import { AboutComponent } from './shared/components/about/about.component';
+import { Pages } from './shared/enums/pages.enum';
+import { ContactComponent } from './shared/components/contact/contact.component';
+import { SchedulingComponent } from './shared/components/scheduling/scheduling.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: Pages.HOME, component: BodyComponent },
+  { path: Pages.ABOUT, component: AboutComponent },
+  { path: Pages.CONTACT, component: ContactComponent },
+  { path: Pages.SCHEDULING, component: SchedulingComponent },
+  { path: Pages.BIO, component: BiographyComponent },
+  { path: `${Pages.BIO}/:name`, component: BiographyComponent },
+  { path: '**', redirectTo: '' },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)], // Configure the routes
+  exports: [RouterModule], // Export RouterModule so it can be used in AppModule
+})
+export class AppRoutingModule {}
