f8875dcec33be956b9eb8649ed8931c318531b71
Diego Ponte <d.ponte@deli-home.com>
2026-08-04 22:37:22 +0100

    api-url for staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://care-be-staging.fly.dev',
+  apiUrl: 'https://api-staging.careclinica.com',
 
   /**
    * Sem integração de pagamento nesta fase. O seletor de método existe e é
